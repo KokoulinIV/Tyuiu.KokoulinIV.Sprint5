28697fd13c21f5d02273b97cfbb8884fe97bd448
KokoulinIV <wolfff1309@yandex.ru>
2024-11-19 22:51:49 +0500

    Task0

--- Tyuiu.KokoulinIV.Sprint5.Task0.V21.Lib/DataService.cs
+++ Tyuiu.KokoulinIV.Sprint5.Task0.V21.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.KokoulinIV.Sprint5.Task0.V21.Lib
         {
             string path = Path.GetTempFileName();
             double y = (x*x+1)/(Math.Pow(4*x*x-3, 0.5));
-            y = Math.Round(y,2);
+            y = Math.Round(y,3);
             File.WriteAllText(path, Convert.ToString(y));
             return path;
         }
