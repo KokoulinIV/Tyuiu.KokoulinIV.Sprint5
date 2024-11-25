1ca599d8ca56dd9ca33b6a71c8522a479f592453
KokoulinIV <wolfff1309@yandex.ru>
2024-11-25 17:17:08 +0500

    Task3

---
+++ Tyuiu.KokoulinIV.Sprint5.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.KokoulinIV.Sprint5.Task3.V2.Lib
+{
+    public class DataService : ISprint5Task3V2
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.txt"); ;
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            double y;
+
+            y = (Math.Pow(Math.E, x)) / x;
+            if (x==0)
+            {
+                y = 0;
+            }
+            y = Math.Round(y,3);
+            using (BinaryWriter write = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), System.Text.Encoding.UTF8))
+            {
+                write.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KokoulinIV.Sprint5.Task3.V2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KokoulinIV.Sprint5.Task3.V2.Lib;
+namespace Tyuiu.KokoulinIV.Sprint5.Task3.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #5 | Выполнил Кокоулин И. В. | ИБКСб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Кокоулин Иван Владимирович   | ИБКСб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов,                    *");
+            Console.WriteLine("* заполненный значениями с клавиатуры. Заменить нечетные                  *");
+            Console.WriteLine("* элементы массива на 0.                                                  *");
+            Console.WriteLine("*  Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 3;
+            Console.WriteLine("x = " + x);
+            string res = ds.SaveToFileTextData(x);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Файл:" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
